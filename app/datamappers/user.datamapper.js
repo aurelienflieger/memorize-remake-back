25a05a8a5de309c474d7142de40279b881c71ebc
aurelien.flieger <aurelien.flieger@protonmail.com>
2024-02-09 13:40:27 +0100

    fix: revert getUserByEmail feature as using the insert method from the CoreDataMapper only takes an id

--- app/datamappers/user.datamapper.js
+++ app/datamappers/user.datamapper.js
@@ -1,3 +1,4 @@
+import pool from "../pg.client.js";
 import CoreDatamapper from "./core.datamapper.js";
 
 /* The methods from the CoreDataMapper are available in addition to those specific to the User. */
@@ -5,4 +6,12 @@ export default class UserDataMapper extends CoreDatamapper {
   static readTableName = "user";
 
   static writeTableName = "user";
+
+  static async getUserByEmail(email) {
+    // eslint-disable-next-line quotes
+    const users = await pool.query('SELECT * FROM "user" WHERE email = $1', [
+      email,
+    ]);
+    return users.rows[0];
+  }
 }
