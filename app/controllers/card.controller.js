57e7bd1f0bc3ef0e129a74964774e33d4e62777c
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-10 16:35:15 +0100

    testing:card route/post/create new card in deck OK

--- app/controllers/card.controller.js
+++ app/controllers/card.controller.js
@@ -3,8 +3,10 @@ import CoreController from "./core.controller.js";
 export default class CardController extends CoreController {
   constructor(Datamapper) {
     super(Datamapper);
+
     this.datamapper = new Datamapper();
     this.getAllCardsByDeckID = this.getAllCardsByDeckID.bind(this);
+    this.createNewCard = this.createNewCard.bind(this);
     this.getByPk = this.getByPk.bind(this);
     this.delete = this.delete.bind(this);
     this.update = this.update.bind(this);
@@ -14,5 +16,13 @@ export default class CardController extends CoreController {
     const { id } = params;
     const rows = await this.datamapper.findAllCardsByDeckID(id);
     res.status(200).json(rows);
+  }
+
+  async createNewCard(req, res) {
+    const deckId = req.params.id;
+    const { body } = req;
+    const card = { ...body, deck_id: deckId };
+    const row = await this.datamapper.insert(card);
+    res.status(200).json(row);
   }
 }
