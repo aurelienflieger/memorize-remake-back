b8346b61242d342612e0489512367b0bf41bec57
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-10 16:26:03 +0100

    testing:deck route/pos/create new deck for user OK

--- app/controllers/deck.controller.js
+++ app/controllers/deck.controller.js
@@ -3,8 +3,10 @@ import CoreController from "./core.controller.js";
 export default class DeckController extends CoreController {
   constructor(Datamapper) {
     super(Datamapper);
+
     this.datamapper = new Datamapper();
     this.getAllDecksByUserID = this.getAllDecksByUserID.bind(this);
+    this.createNewDeck = this.createNewDeck.bind(this);
     this.getByPk = this.getByPk.bind(this);
     this.delete = this.delete.bind(this);
     this.update = this.update.bind(this);
@@ -14,5 +16,13 @@ export default class DeckController extends CoreController {
     const { id } = params;
     const rows = await this.datamapper.findAllDecksByUserID(id);
     res.status(200).json(rows);
+  }
+
+  async createNewDeck(req, res) {
+    const userId = req.params.id;
+    const { body } = req;
+    const deck = { ...body, user_id: userId };
+    const row = await this.datamapper.insert(deck);
+    res.status(200).json(row);
   }
 }
