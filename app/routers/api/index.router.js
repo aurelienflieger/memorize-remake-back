39c5f81c026f1e076402a51b0a9abdd8b9070671
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-12 10:05:28 +0100

    fix: remove mergeParams from index.router.js in routers api

--- app/routers/api/index.router.js
+++ app/routers/api/index.router.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRouter from "./user.router.js";
 import authRouter from "./auth.router.js";
 
-const apiRouter = express.Router({ mergeParams: true });
+const apiRouter = express.Router();
 
 apiRouter.use("/account", userRouter);
 apiRouter.use("/auth", authRouter);
