81b085280fde09e205441d2a537695921744fd42
NicolasSokolowski <nicolas.sokolowski@oclock.school>
2024-02-13 18:13:02 +0100

    Update user method in progress

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -11,7 +11,7 @@ class UserController extends CoreController {
     this.signup = this.signup.bind(this);
     this.getByPk = this.getByPk.bind(this);
     this.delete = this.delete.bind(this);
-    this.update = this.update.bind(this);
+    this.updateAccount = this.updateAccount.bind(this);
   }
 
   async login(req, res) {
@@ -56,6 +56,39 @@ class UserController extends CoreController {
     });
 
     res.status(201).json({ newUser });
+  }
+
+  async updateAccount({ params, body }, res) {
+    const { id } = params;
+    const { username, email } = body;
+    const data = await this.datamapper.findByPk(id);
+
+    /* const validPassword = await bcrypt.compare(password, data.password);
+
+    if (!validPassword) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+    const test = await bcrypt.compare(newPassword, data.password);
+    console.log(test);
+
+    console.log(data.password);
+    console.log(newHashedPassword);
+    */
+
+    const isModified = data.username !== username || data.email !== email;
+
+    if (!isModified) {
+      throw new Error("You need to change at least one field");
+    }
+
+    const newAccountInfo = { ...data, email: body.email, username: body.username };
+    console.log(newAccountInfo);
+
+    const row = await this.datamapper.update(newAccountInfo);
+
+    return res.status(200).json(row);
   }
 }
 
