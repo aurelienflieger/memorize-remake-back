93df61754bc5bfc1a96fdae10d0a6304da6b687c
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-15 14:53:19 +0100

    feat: link database to external server

--- app/database/pg.client.js
+++ app/database/pg.client.js
@@ -6,11 +6,9 @@ import pg from "pg";
 const { Pool } = pg;
 
 const poolConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DB,
+  connectionString: process.env.PG_URL,
+  ssl: { rejectUnauthorized:false },
+
 };
 
 const pool = new Pool(poolConfig);
