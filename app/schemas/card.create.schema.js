e982c440530951c4707d2505c0f0222ee15af742
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-17 09:21:19 +0100

    fix: remove deck_id, difficulty fields from card creation schema

--- app/schemas/card.create.schema.js
+++ app/schemas/card.create.schema.js
@@ -3,6 +3,4 @@ import Joi from "joi";
 export default Joi.object({
   "front": Joi.string(),
   "back": Joi.string(),
-  "difficulty": Joi.number().default(0).exist(),
-  "deck_id": Joi.number().exist(),
 });
