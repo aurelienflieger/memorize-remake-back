b68cf54f0d55edf990fb1ff623a77897e9b86e23
NicolasSokolowski <nicolas.sokolowski@oclock.school>
2024-02-18 15:59:57 +0100

    fix: delete console.log

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -98,7 +98,6 @@ class UserController extends CoreController {
     }
 
     const newHashedPassword = await bcrypt.hash(newPassword, 10);
-    console.log(newHashedPassword);
 
     const comparePasswords = await bcrypt.compare(newPassword, data.password);
 
