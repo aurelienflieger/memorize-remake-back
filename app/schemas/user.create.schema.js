b5e1930614908a802f360a63e798694a407f0668
NicolasSokolowski <nicolas.sokolowski@oclock.school>
2024-02-18 15:46:45 +0100

    feat: user password update

--- app/schemas/user.create.schema.js
+++ app/schemas/user.create.schema.js
@@ -4,15 +4,19 @@ export default Joi.object({
   username: Joi.string()
     .max(15)
     .messages({
-      "string.base": "The username has to be of type \"text\"",
+      "string.base": "The username has to be of type TEXT.",
       "string.max": "The username has to be 15 characters or less."
     }),
   email: Joi.string()
     .email({ minDomainSegments: 2 })
     .messages({
-      "string.base": "The adress has to be of type \"text\"",
+      "string.base": "The adress has to be of type TEXT.",
       "string.email": "Please enter a valid email address."
     }
     ),
   password: Joi.string()
+    .min(6)
+    .messages({
+      "string.min": "The password needs 6 characters or more."
+    })
 }).required();
