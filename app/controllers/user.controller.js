2b18dc5eff9dd4969ad2675966108e22543ef324
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-20 12:42:59 +0100

    fix: import & bracket error in user controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -4,7 +4,6 @@ import CoreController from "./core.controller.js";
 import { UserDataMapper } from "../datamappers/index.datamapper.js";
 import ApiError from "../errors/api.error.js";
 
-
 class UserController extends CoreController {
   constructor() {
     const datamapper = new UserDataMapper();
@@ -26,7 +25,7 @@ class UserController extends CoreController {
     const validPassword = await bcrypt.compare(inputPassword, user.password);
 
     if (!validPassword) {
-      throw new ApiError("Incorrect password", { httpStatus: 400 })
+      throw new ApiError("Incorrect password", { httpStatus: 400 });
     }
 
     const tokens = generateJWT(user);
@@ -73,58 +72,62 @@ class UserController extends CoreController {
     const isModified = data.username === username && data.email === email;
 
     if (isModified) {
-      throw new ApiError("You need to change at least one field", { httpStatus: 400 });
+      throw new ApiError("You need to change at least one field", {
+        httpStatus: 400,
+      });
     }
 
-    const newAccountInfo = { ...data, email: email, username: username};
+    const newAccountInfo = { ...data, email: email, username: username };
 
     const row = await this.datamapper.update(newAccountInfo);
 
     return res.status(200).json(row);
-  }
-  };
-
-  updateAccountPassword = async ({ params, body }, res) => {
-    const { id } = params;
-    let { password, newPassword } = body;
-    const data = await this.datamapper.findByPk(id);
-
-    if (!data) {
-      throw new ApiError("This account does not exist.", { httpStatus: 404 });
-    }
-
-    const validPassword = await bcrypt.compare(password, data.password);
-
-    if (!validPassword) {
-      throw new ApiError("Incorrect password", { httpStatus: 400 })
-    }
-
-    const newHashedPassword = await bcrypt.hash(newPassword, 10);
-
-    const comparePasswords = await bcrypt.compare(newPassword, data.password);
-
-    if (comparePasswords) {
-      throw new ApiError("Your current password and the new one must be different.", { httpStatus: 400 })
-    }
-
-    const newAccountPassword = { ...data, password: newHashedPassword };
-
-    const row = await this.datamapper.update(newAccountPassword);
-
-    return res.status(200).json(row);
-  }
-
-  getByPk = async ({ params }, res) => {
-    const { id } = params;
-
-    const row = await this.datamapper.findByPkWithNoReturnedPassword(id);
-
-    if (row === undefined) {
-      throw new ApiError("This account does not exist.", { httpStatus: 404 });
-    };
-
-    return res.status(201).json(row);
   };
 }
+
+updateAccountPassword = async ({ params, body }, res) => {
+  const { id } = params;
+  let { password, newPassword } = body;
+  const data = await this.datamapper.findByPk(id);
+
+  if (!data) {
+    throw new ApiError("This account does not exist.", { httpStatus: 404 });
+  }
+
+  const validPassword = await bcrypt.compare(password, data.password);
+
+  if (!validPassword) {
+    throw new ApiError("Incorrect password", { httpStatus: 400 });
+  }
+
+  const newHashedPassword = await bcrypt.hash(newPassword, 10);
+
+  const comparePasswords = await bcrypt.compare(newPassword, data.password);
+
+  if (comparePasswords) {
+    throw new ApiError(
+      "Your current password and the new one must be different.",
+      { httpStatus: 400 }
+    );
+  }
+
+  const newAccountPassword = { ...data, password: newHashedPassword };
+
+  const row = await this.datamapper.update(newAccountPassword);
+
+  return res.status(200).json(row);
+};
+
+getByPk = async ({ params }, res) => {
+  const { id } = params;
+
+  const row = await this.datamapper.findByPkWithNoReturnedPassword(id);
+
+  if (row === undefined) {
+    throw new ApiError("This account does not exist.", { httpStatus: 404 });
+  }
+
+  return res.status(201).json(row);
+};
 
 export default UserController;
