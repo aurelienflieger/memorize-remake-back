5a6b03ad1359676e98db9924ce06588be687da39
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-20 10:34:16 +0100

    fix: add back controllerWrapper to auth router for fully working error handling
    Note that the error patterns for the user creation schema are not yet returned as a group, only individually.

--- app/routers/api/auth.router.js
+++ app/routers/api/auth.router.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { userController } from "../../controllers/index.controller.js";
+import { controllerWrapper } from "../../utils/index.util.js";
 
 const authRouter = express.Router();
 
-authRouter.route("/")
-  .post(userController.login);
+authRouter.route("/").post(controllerWrapper(userController.login));
 
 export default authRouter;
