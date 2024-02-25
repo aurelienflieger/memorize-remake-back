a95570764c69da64f37733dbf085c34d4ce93bdf
aurelienflieger <aurelien.flieger@protonmail.com>
2024-02-25 12:04:59 +0100

    build: simplify db_reset script to remove extra steps

--- app/middlewares/checkForValidAuthentification.middleware.js
+++ app/middlewares/checkForValidAuthentification.middleware.js
@@ -11,16 +11,13 @@ const checkForValidAuthentification = async (req, res, next) => {
     });
   }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    console.log(err);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
     if (err) {
       return res.status(500).json({
         error:
           "The token provided is invalid. There may be an issue with your account. Please contact support.",
       });
     }
-
-    console.log(decoded);
 
     next();
   });
