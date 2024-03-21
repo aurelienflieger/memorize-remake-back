79aee65670dff4ce321f1f7be4dde66890b6e753
aurelienflieger <aurelien.flieger@protonmail.com>
2024-03-21 14:21:37 +0100

    feat: add validation middleware for schema verification in login route

--- app/routers/api/auth.router.js
+++ app/routers/api/auth.router.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { userController } from "../../controllers/index.controller.js";
 import { controllerWrapper } from "../../utils/index.util.js";
+import { validateInput } from "../../middlewares/index.middleware.js";
+import authLoginSchema from "../../schemas/auth.login.schema.js";
 
 const authRouter = express.Router();
 
-authRouter.route("/").post(controllerWrapper(userController.login));
+authRouter
+  .route("/")
+  .post(
+    validateInput("body", authLoginSchema),
+    controllerWrapper(userController.login)
+  );
 
 /* authRouter
   .route("/refresh-tokens")
